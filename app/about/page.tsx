c843f9bf6b3f6c58ba8803756538281da0bf3f0b
Mel-Turham <tchoikuemel06@gmail.com>
2025-02-08 16:07:48 +0100

    feat(about page): create AboutPage component with SectionHero

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import SectionHero from "@/sections/section-hero";
+
+const AboutPage = () => {
+  return (
+    <main className="min-h-screen grow">
+      <SectionHero />
+    </main>
+  );
+};
+
+export default AboutPage;
