7f526fd337196f084282298127e1b68c8e01dbd5
Mel-Turham <tchoikuemel06@gmail.com>
2025-02-08 15:59:55 +0100

    feat(services page): ajout de la page services section et les differentes section

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,10 +1,12 @@
 import SectionHero from "@/sections/section-hero";
+import SectionServices from "@/sections/section-services";
 import React from "react";
 
 const ServicesPages = () => {
   return (
     <main className="min-h-screen grow">
       <SectionHero />
+      <SectionServices />
     </main>
   );
 };
