569cf81a45f41b4c5cf24efb6aedb91b8b2acee2
Mel-Turham <tchoikuemel06@gmail.com>
2025-02-12 09:52:39 +0100

    feat(about page): enhance AboutPage with new sections for history, missions, and team

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,17 @@
+import HistorySection from "@/sections/history-section";
+import AboutSection from "@/sections/section-about";
+import SectionEquipes from "@/sections/section-equipes";
 import SectionHero from "@/sections/section-hero";
+import SectionMissions from "@/sections/section-missions";
 
 const AboutPage = () => {
   return (
     <main className="min-h-screen grow">
       <SectionHero />
+      <AboutSection />
+      <HistorySection />
+      <SectionMissions />
+      <SectionEquipes />
     </main>
   );
 };
