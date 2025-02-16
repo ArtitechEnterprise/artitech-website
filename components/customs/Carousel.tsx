1fb9ed0a87abf645f0a70544a53596f3becd12dc
Mel-Turham <tchoikuemel06@gmail.com>
2025-02-16 12:30:31 +0100

    feat(footer): update footer layout and styling for improved responsiveness and theme support

--- components/customs/Carousel.tsx
+++ components/customs/Carousel.tsx
@@ -1,35 +1,241 @@
-import React from "react";
-import CarouselData from "@/data/carousel";
-import { Card, CardContent, CardDescription, CardFooter } from "../ui/card";
-import Image from "next/image";
-import { Button } from "../ui/button";
-const Carousel = () => {
+"use client";
+import { ArrowRight } from "lucide-react";
+import { useState, useRef, useId, useEffect, useCallback } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+interface SlideData {
+  title: string;
+  button: string;
+  src: string;
+  description: string;
+}
+
+interface SlideProps {
+  slide: SlideData;
+  index: number;
+  current: number;
+  handleSlideClick: (index: number) => void;
+}
+
+const Slide = ({ slide, index, current, handleSlideClick }: SlideProps) => {
+  const slideRef = useRef<HTMLLIElement>(null);
+
+  const xRef = useRef(0);
+  const yRef = useRef(0);
+  const frameRef = useRef<number>(0);
+
+  useEffect(() => {
+    const animate = () => {
+      if (!slideRef.current) return;
+
+      const x = xRef.current;
+      const y = yRef.current;
+
+      slideRef.current.style.setProperty("--x", `${x}px`);
+      slideRef.current.style.setProperty("--y", `${y}px`);
+
+      frameRef.current = requestAnimationFrame(animate);
+    };
+
+    frameRef.current = requestAnimationFrame(animate);
+
+    return () => {
+      if (frameRef.current) {
+        cancelAnimationFrame(frameRef.current);
+      }
+    };
+  }, []);
+
+  const handleMouseMove = (event: React.MouseEvent) => {
+    const el = slideRef.current;
+    if (!el) return;
+
+    const r = el.getBoundingClientRect();
+    xRef.current = event.clientX - (r.left + Math.floor(r.width / 2));
+    yRef.current = event.clientY - (r.top + Math.floor(r.height / 2));
+  };
+
+  const handleMouseLeave = () => {
+    xRef.current = 0;
+    yRef.current = 0;
+  };
+
+  const imageLoaded = (event: React.SyntheticEvent<HTMLImageElement>) => {
+    event.currentTarget.style.opacity = "1";
+  };
+
+  const { src, button, title, description } = slide;
+
   return (
-    <>
-      {CarouselData.map((item) => (
-        <Card className="space-y-4 border-none shadow-none" key={item.id}>
-          <CardContent className="h-[230px] overflow-hidden rounded-3xl bg-red-600 px-0">
-            <Image
-              width={400}
-              height={400}
-              src={item.image}
-              alt={item.type}
-              loading="lazy"
-              title={item.type}
-            />
-          </CardContent>
-          <CardFooter className="flex-col items-start gap-3 px-0">
-            <CardDescription className="text-base font-semibold text-black/70 dark:text-white">
-              {item.description}
-            </CardDescription>
-            <Button className="rounded-full" size={"lg"} variant={"secondary"}>
-              {item.type}
-            </Button>
-          </CardFooter>
-        </Card>
-      ))}
-    </>
+    <div className="[perspective:1200px] [transform-style:preserve-3d]">
+      <li
+        ref={slideRef}
+        className="relative z-10 mx-[4vmin] flex h-[70vmin] w-[70vmin] flex-1 flex-col items-center justify-center text-center text-white opacity-100 transition-all duration-300 ease-in-out"
+        onClick={() => handleSlideClick(index)}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+        style={{
+          transform:
+            current !== index
+              ? "scale(0.98) rotateX(8deg)"
+              : "scale(1) rotateX(0deg)",
+          transition: "transform 0.5s cubic-bezier(0.4, 0, 0.2, 1)",
+          transformOrigin: "bottom",
+        }}
+      >
+        <div
+          className="absolute left-0 top-0 h-full w-full overflow-hidden rounded-[1%] bg-[#1D1F2F] transition-all duration-150 ease-out"
+          style={{
+            transform:
+              current === index
+                ? "translate3d(calc(var(--x) / 30), calc(var(--y) / 30), 0)"
+                : "none",
+          }}
+        >
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            className="duration-600 absolute inset-0 h-[120%] w-[120%] object-cover opacity-100 transition-opacity ease-in-out"
+            style={{
+              opacity: current === index ? 1 : 0.5,
+            }}
+            alt={title}
+            src={src}
+            onLoad={imageLoaded}
+            loading="eager"
+            decoding="sync"
+          />
+          {current === index && (
+            <div className="absolute inset-0 bg-black/30 transition-all duration-1000" />
+          )}
+        </div>
+
+        <AnimatePresence key={current}>
+          <article
+            className={`relative z-10 p-[4vmin] transition-opacity duration-1000 ease-in-out ${
+              current === index ? "visible opacity-100" : "invisible opacity-0"
+            }`}
+          >
+            <motion.h2
+              className="relative bg-clip-text text-sm font-light"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              {title}
+            </motion.h2>
+            <motion.p
+              className="relative my-3 text-lg font-semibold md:text-lg lg:text-xl"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.2 }}
+            >
+              {description}
+            </motion.p>
+            <div className="flex justify-center">
+              <button className="mx-auto mt-6 flex h-12 w-fit items-center justify-center rounded-2xl border border-transparent bg-white px-4 py-2 text-xs text-black shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] transition duration-200 hover:shadow-lg sm:text-sm">
+                {button}
+              </button>
+            </div>
+          </article>
+        </AnimatePresence>
+        <div className="absolute inset-0 h-full w-full bg-black/55"></div>
+      </li>
+    </div>
   );
 };
 
-export default Carousel;
+interface CarouselControlProps {
+  type: string;
+  title: string;
+  handleClick: () => void;
+}
+
+const CarouselControl = ({
+  type,
+  title,
+  handleClick,
+}: CarouselControlProps) => {
+  return (
+    <button
+      className={`border-3 mx-2 flex h-10 w-10 items-center justify-center rounded-full border-transparent bg-neutral-200 transition duration-200 hover:-translate-y-0.5 focus:border-[#6D64F7] focus:outline-none active:translate-y-0.5 dark:bg-neutral-800 ${
+        type === "previous" ? "rotate-180" : ""
+      }`}
+      title={title}
+      onClick={handleClick}
+    >
+      <ArrowRight className="text-neutral-600 dark:text-neutral-200" />
+    </button>
+  );
+};
+
+interface CarouselProps {
+  slides: SlideData[];
+}
+
+export function Carousel({ slides }: CarouselProps) {
+  const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const next = current + 1;
+      setCurrent(next === slides.length ? 0 : next);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [current, slides.length]);
+
+  const handlePreviousClick = useCallback(() => {
+    const previous = current - 1;
+    setCurrent(previous < 0 ? slides.length - 1 : previous);
+  }, [current, slides.length]);
+
+  const handleNextClick = useCallback(() => {
+    const next = current + 1;
+    setCurrent(next === slides.length ? 0 : next);
+  }, [current, slides.length]);
+
+  const handleSlideClick = (index: number) => {
+    if (current !== index) {
+      setCurrent(index);
+    }
+  };
+
+  const id = useId();
+
+  return (
+    <div
+      className="relative mx-auto h-[70vmin] w-[70vmin]"
+      aria-labelledby={`carousel-heading-${id}`}
+    >
+      <ul
+        className="absolute mx-[-4vmin] flex transition-transform duration-1000 ease-in-out"
+        style={{
+          transform: `translateX(-${current * (100 / slides.length)}%)`,
+        }}
+      >
+        {slides.map((slide, index) => (
+          <Slide
+            key={index}
+            slide={slide}
+            index={index}
+            current={current}
+            handleSlideClick={handleSlideClick}
+          />
+        ))}
+      </ul>
+
+      <div className="absolute bottom-3 flex w-full justify-center">
+        <CarouselControl
+          type="previous"
+          title="Go to previous slide"
+          handleClick={handlePreviousClick}
+        />
+
+        <CarouselControl
+          type="next"
+          title="Go to next slide"
+          handleClick={handleNextClick}
+        />
+      </div>
+    </div>
+  );
+}
--- components/customs/footer.tsx
+++ components/customs/footer.tsx
@@ -1,29 +1,32 @@
-import React from "react";
+"use client";
 import Container from "./container";
 import { MapPin, Phone } from "lucide-react";
 import Links from "@/data/links";
 import Link from "next/link";
 import Socials from "./socials";
 import Image from "next/image";
+import { useTheme } from "next-themes";
 
 const Footer = () => {
+  const { theme } = useTheme();
+
   return (
-    <footer className="mt-10 bg-[#3D3D3D] py-8 text-white">
+    <footer className="mt-10 border-t-2 bg-card py-8">
       <Container className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         <div className="">
           {/*  */}
-          <h3 className="text-4xl font-semibold">Canada</h3>
+          <h3 className="text-xl font-semibold">Canada</h3>
           <div className="mt-4 space-y-3">
-            <p className="flex items-center gap-2 text-white dark:text-muted-foreground">
+            <p className="flex items-center gap-2">
               <MapPin className="h-6 w-6" strokeWidth={1.5} />
               <span className="font-light">1234 Rue de la Technologie</span>
             </p>
-            <p className="flex items-center gap-2 text-white dark:text-muted-foreground">
+            <p className="flex items-center gap-2">
               <MapPin className="h-6 w-6" strokeWidth={1.5} />
 
               <span>Montréal, QC H3A 2B4</span>
             </p>
-            <p className="flex items-center gap-2 text-white dark:text-muted-foreground">
+            <p className="flex items-center gap-2">
               <Phone className="h-6 w-6" strokeWidth={1.5} />
               <span>Tél : +1 514-123-4567</span>
             </p>
@@ -31,18 +34,18 @@ const Footer = () => {
         </div>
         <div className="">
           {/*  */}
-          <h3 className="text-4xl font-semibold">Cameroun</h3>
+          <h3 className="text-xl font-semibold">Cameroun</h3>
           <div className="mt-4 space-y-3">
-            <p className="flex items-center gap-2 text-white dark:text-muted-foreground">
+            <p className="flex items-center gap-2">
               <MapPin className="h-6 w-6" strokeWidth={1.5} />
               <span className="font-light">456 Rue de la Côte</span>
             </p>
-            <p className="flex items-center gap-2 text-white dark:text-muted-foreground">
+            <p className="flex items-center gap-2">
               <MapPin className="h-6 w-6" strokeWidth={1.5} />
 
               <span>Douala, Littoral, Cameroun</span>
             </p>
-            <p className="flex items-center gap-2 text-white dark:text-muted-foreground">
+            <p className="flex items-center gap-2">
               <Phone className="h-6 w-6" strokeWidth={1.5} />
               <span>Tél : +237 6 96 35 41 28</span>
             </p>
@@ -50,7 +53,7 @@ const Footer = () => {
         </div>
         <div className="">
           {/*  */}
-          <h3 className="text-4xl font-semibold">Menus</h3>
+          <h3 className="text-xl font-semibold">Menus</h3>
           <ul className="mt-4 space-y-3">
             {Links.map((link) => (
               <li key={link.href}>
@@ -60,11 +63,10 @@ const Footer = () => {
           </ul>
         </div>
         <div className="">
-          {/*  */}
-          <h3 className="text-4xl font-semibold">Follow us</h3>
+          <h3 className="text-xl font-semibold">Follow us</h3>
           <div className="mt-4 space-y-3">
             <Socials className="px-2 py-2" />
-            <div className="flex items-center justify-center gap-3">
+            <div className="flex items-center gap-3 lg:justify-center">
               <p className="text-xs font-light">Nous rejoindre</p>
               <p className="text-xs font-light">Contact</p>
               <p className="text-xs font-light">Mentions légales</p>
@@ -72,19 +74,28 @@ const Footer = () => {
           </div>
         </div>
       </Container>
-      <Container className="flex gap-9">
-        <div className="self-center">
-          <Image
-            src={"/logo-dark.jpg"}
-            alt="logo"
-            width={100}
-            height={100}
-            loading="lazy"
-            className="w-24"
-          />
+      <Container className="flex flex-col gap-9 lg:flex-row">
+        <div className="md:self-center">
+          {theme === "light" ? (
+            <Image
+              src="/logo-dark.png"
+              width={100}
+              height={100}
+              alt="Aritech"
+              loading="lazy"
+            />
+          ) : (
+            <Image
+              src="/logo-light.png"
+              width={100}
+              height={100}
+              alt="Aritech"
+              loading="lazy"
+            />
+          )}
           <span>Les artisants du digital</span>
         </div>
-        <p className="w-3/4 self-end justify-self-center text-center">
+        <p className="justify-self-center lg:w-3/4 lg:self-end lg:text-center">
           © 2024 Artitech. Tous droits réservés.
         </p>
       </Container>
