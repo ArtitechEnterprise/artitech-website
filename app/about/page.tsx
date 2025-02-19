3df3e416105f5b561d3b697f29e94c730b225553
Mel-Turham <tchoikuemel06@gmail.com>
2025-02-19 18:11:50 +0100

    feat(about): refactor AboutPage to use new section components for improved structure

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,17 @@
-import HistorySection from "@/sections/history-section";
-import AboutSection from "@/sections/section-about";
-import SectionEquipes from "@/sections/section-equipes";
-import SectionHero from "@/sections/section-hero";
-import SectionMissions from "@/sections/section-missions";
+import Equipes from "@/sections/about-equipes";
+import { AboutHeroSection } from "@/sections/about-hero-section";
+import AboutHistorySection from "@/sections/about-history-section";
+import AboutImpactEtValeurs from "@/sections/about-impact-valeur";
+import AboutMissionVision from "@/sections/about-mission-vision";
 
 const AboutPage = () => {
   return (
     <main className="min-h-screen grow">
-      <SectionHero />
-      <AboutSection />
-      <HistorySection />
-      <SectionMissions />
-      <SectionEquipes />
+      <AboutHeroSection />
+      <AboutHistorySection />
+      <AboutMissionVision />
+      <Equipes />
+      <AboutImpactEtValeurs />
     </main>
   );
 };
