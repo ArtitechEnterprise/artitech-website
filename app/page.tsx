f3a856edb0ec2aac9e3d84919725f7be40b2967c
Mel-Turham <tchoikuemel06@gmail.com>
2025-02-19 18:11:36 +0100

    feat(home): replace hero and socials sections with HomeHeroSection component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
+import { HomeHeroSection } from "@/sections/home-hero-section";
 import SectionAvis from "@/sections/section-avis";
 import SectionFaq from "@/sections/section-faq";
-import SectionHero from "@/sections/section-hero";
 import SectionRealisation from "@/sections/section-realisations";
-import SectionSocials from "@/sections/section-socials";
-
 import React from "react";
 
 const HomePage = () => {
   return (
     <main className="min-h-screen grow">
-      <SectionHero />
-      <SectionSocials />
+      <HomeHeroSection />
       <SectionRealisation />
       <SectionAvis />
       <SectionFaq />
