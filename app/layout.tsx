17a1ba739fdce15dc11bee42859c4959e327ad4e
Mel-Turham <tchoikuemel06@gmail.com>
2025-02-19 18:11:19 +0100

    feat(layout): enhance metadata and add loading animation and scroll indicator components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,55 @@
 import type { Metadata } from "next";
-import { IBM_Plex_Mono } from "next/font/google";
+
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 
 import Footer from "@/components/customs/footer";
 import Navbar from "@/components/customs/navbar";
+import IndicatorOnScroll from "@/components/customs/indicator-scroll";
+import { LoadingAnimation } from "@/components/customs/loading-ainmation";
 
 export const metadata: Metadata = {
   title: "Home | Artitech ",
   description:
     "Artitech entreprise de développement d'application web et mobile à Douala et Canada",
+  openGraph: {
+    title:
+      "ARTITECH | Artitech entreprise de développement d'application web et mobile à Douala et Canada",
+    description:
+      "Solutions digitales qui propulsent votre entreprise a l'avenir",
+    url: "",
+    siteName:
+      "ARTITECH | Artitech entreprise de développement d'application web et mobile à Douala et Canada",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dd2t9tdyy/image/upload/v1739702462/rgum1rvqdp5vdazhuco7.jpg",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "fr_FR",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  twitter: {
+    card: "summary_large_image",
+    title:
+      "ARTITECH | Artitech entreprise de développement d'application web et mobile à Douala et Canada",
+    description:
+      "Solutions digitales qui propulsent votre entreprise a l'avenir",
+    images: [
+      "https://res.cloudinary.com/dd2t9tdyy/image/upload/v1739702462/rgum1rvqdp5vdazhuco7.jpg",
+    ],
+  },
 };
-
-const ibmPlexMono = IBM_Plex_Mono({
-  weight: ["100", "200", "300", "400", "500", "600", "700"],
-  subsets: ["latin"],
-});
 
 export default function RootLayout({
   children,
@@ -24,13 +57,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${ibmPlexMono.className} antialiased`}>
+    <html lang="fr" suppressHydrationWarning>
+      <body className={`antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
           disableTransitionOnChange
         >
+          <LoadingAnimation />
+          <IndicatorOnScroll />
           <Navbar />
           {children}
           <Toaster />
